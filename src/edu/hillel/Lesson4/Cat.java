8ef5b4ca446f69d88dc1e943fa861b4553f62eb6
Тимощенко Михаил <michaeltim@gmail.com>
2023-08-15 21:54:33 +0200

    lesson 4 static!!

--- src/edu/hillel/Lesson4/Cat.java
+++ src/edu/hillel/Lesson4/Cat.java
@@ -2,8 +2,8 @@ package edu.hillel.Lesson4;
 
 public class Cat extends Animal{
 
-    static int RUN_THRESHOLD=200;
-    static int SWIM_THRESHOLD=0;
+    final static int RUN_THRESHOLD=200;
+    final static int SWIM_THRESHOLD=0;
     public Cat(Counter counter,String name) {
         super(counter,name,RUN_THRESHOLD,SWIM_THRESHOLD);
         counter.catIncrement();
