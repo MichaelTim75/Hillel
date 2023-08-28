38282f89d1015585180794bbaf956d9af111f2c5
Тимощенко Михаил <michaeltim@gmail.com>
2023-08-28 22:04:37 +0200

    lesson 8; added throwing Exception when array is empty added ArithmeticException when division by zero

--- src/edu/hillel/Lesson8/ArrayValueCalculator.java
+++ src/edu/hillel/Lesson8/ArrayValueCalculator.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 public class ArrayValueCalculator {
     final static private int ARRAY_DIMENSION_THRESHOLD=4;
 
-    public int doCalc(String[][] arr) throws ArraySizeException, ArrayDataException {
+    //first method throws ArraySizeException,ArrayDataException.
+    // But I try to add reaction on another global exception - and now method throws global Exception because my exceptions extends of Exception
+    public int doCalc(String[][] arr) throws Exception {
         if (arr.length>ARRAY_DIMENSION_THRESHOLD){
             throw new ArraySizeException("Dimension of array "+arr.length+" is over limit="+ARRAY_DIMENSION_THRESHOLD);
+        } else if (arr.length==0) {
+            throw new Exception("Input array is empty!!");
+
         }
         for (int i=0;i<arr.length;i++){
             if (arr[i].length>ARRAY_DIMENSION_THRESHOLD){
@@ -24,6 +29,9 @@ public class ArrayValueCalculator {
                     throw new ArrayDataException("Data in row "+i+" and column "+j+" is \""+arr[i][j]+"\" and cannot be parsed to int!");
                 }
             }
+        }
+        if (arr[1][1].equals("0")){
+            totalSum=totalSum/Integer.parseInt(arr[1][1]);
         }
         return totalSum;
     }
--- src/edu/hillel/Lesson8/Main.java
+++ src/edu/hillel/Lesson8/Main.java
@@ -1,6 +1,8 @@
 package edu.hillel.Lesson8;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +15,13 @@ public class Main {
         } catch (ArraySizeException|ArrayDataException e) {
             System.out.println("Throwed exception "+e.getClass()+", message = "+e.getMessage());
         }
+        catch (Exception e){
+            System.out.println("Got an exception of "+e.getClass());
+            StringWriter stringWriter=new StringWriter();
+            PrintWriter printWriter=new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
+            System.out.println(stringWriter.toString());
+        }
 
         //demonstration of ArrayDataException
         String[][] arr2={{"12","24","48","7"},{"12","5","3","99"},{"1","2","as","7"},{"12","24","as","7"}};
@@ -21,6 +30,13 @@ public class Main {
         } catch (ArraySizeException|ArrayDataException e) {
             System.out.println("Throwed exception "+e.getClass()+", message = "+e.getMessage());
         }
+        catch (Exception e){
+            System.out.println("Got an exception of "+e.getClass());
+            StringWriter stringWriter=new StringWriter();
+            PrintWriter printWriter=new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
+            System.out.println(stringWriter.toString());
+        }
 
         //correct version
         String[][] arr3={{"12","24","48","7"},{"12","5","3","99"},{"1","2","5","7"},{"12","24","96","7"}};
@@ -28,6 +44,43 @@ public class Main {
             System.out.println("Result of total sum in dimension is "+arrayValueCalculator.doCalc(arr3));
         } catch (ArraySizeException|ArrayDataException e) {
             System.out.println("Throwed exception "+e.getClass()+", message = "+e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println("Got an exception of "+e.getClass());
+            StringWriter stringWriter=new StringWriter();
+            PrintWriter printWriter=new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
+            System.out.println(stringWriter.toString());
+        }
+
+        //throw Exception
+        String[][] arr4={};
+        try {
+            System.out.println("Result of total sum in dimension is "+arrayValueCalculator.doCalc(arr4));
+        } catch (ArraySizeException|ArrayDataException e) {
+            System.out.println("Throwed exception "+e.getClass()+", message = "+e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println("Got an exception of "+e.getClass());
+            StringWriter stringWriter=new StringWriter();
+            PrintWriter printWriter=new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
+            System.out.println(stringWriter.toString());
+        }
+
+        //throws division by zero
+        String[][] arr5={{"12","24","48","7"},{"1","0","3","99"},{"1","2","5","7"},{"12","24","96","7"}};
+        try {
+            System.out.println("Result of total sum in dimension is "+arrayValueCalculator.doCalc(arr5));
+        } catch (ArraySizeException|ArrayDataException e) {
+            System.out.println("Throwed exception "+e.getClass()+", message = "+e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println("Got an exception of "+e.getClass());
+            StringWriter stringWriter=new StringWriter();
+            PrintWriter printWriter=new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
+            System.out.println(stringWriter.toString());
         }
 
     }
