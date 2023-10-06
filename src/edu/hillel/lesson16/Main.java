17b0a5814e94bb1957fa4239f764f2fde983a55d
Тимощенко Михаил <michaeltim@gmail.com>
2023-10-06 14:26:32 +0200

    Lesson 16 third realization - with and without synchro 5 threads with CyclicBarrier

--- src/edu/hillel/lesson16/Main.java
+++ src/edu/hillel/lesson16/Main.java
@@ -7,19 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         ValueCalculator valueCalculator = new ValueCalculator();
-        OptionalDouble avgTiming1 = LongStream.rangeClosed(1,100).
-                map(time ->{
+        valueCalculator.method(true);
+
+        OptionalDouble avgTiming1 = LongStream.rangeClosed(1, 100).
+                map(time -> {
                     return valueCalculator.method(true);
-                }). average();
+                }).average();
 
-        System.out.println("With synchronization, 100 times by 10 million - avg timing is "+avgTiming1.getAsDouble());
+        System.out.println("With synchronization, 100 times by 10 million - avg timing is " + avgTiming1.getAsDouble());
 
-        OptionalDouble avgTiming2 = LongStream.rangeClosed(1,100).
-                map(time ->{
+        OptionalDouble avgTiming2 = LongStream.rangeClosed(1, 100).
+                map(time -> {
                     return valueCalculator.method(false);
-                }). average();
+                }).average();
 
-        System.out.println("Without synchronization, 100 times by 10 million - avg timing is "+avgTiming2.getAsDouble());
+        System.out.println("Without synchronization, 100 times by 10 million - avg timing is " + avgTiming2.getAsDouble());
 
     }
 }
--- src/edu/hillel/lesson16/ReCalcArray.java
+++ src/edu/hillel/lesson16/ReCalcArray.java
@@ -1,12 +1,19 @@
 package edu.hillel.lesson16;
 
-public class ReCalcArray implements Runnable{
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class ReCalcArray implements Runnable {
     private final float[] array;
     private final boolean needSynchro;
+    private final CyclicBarrier barrier;
+    private final String name;
 
-    public ReCalcArray(float[] array, boolean needSynchro) {
+    public ReCalcArray(float[] array, boolean needSynchro, CyclicBarrier barrier, String name) {
         this.array = array;
         this.needSynchro = needSynchro;
+        this.barrier = barrier;
+        this.name = name;
     }
 
     @Override
@@ -17,11 +24,17 @@ public class ReCalcArray implements Runnable{
                     array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
-        }
-        else {
+        } else {
             for (int i = 0; i < array.length; i++) {
                 array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         }
+
+        try {
+            this.barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
